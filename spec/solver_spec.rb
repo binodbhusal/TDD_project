98fceaa5bc6b8e9361adddbfc9890b0c293d2523
David Andrianarivo <andrianarivo.david@gmail.com>
2023-08-03 20:11:01 +0300

    refactor

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,44 +1,52 @@
 require 'rspec'
 require_relative '../solver'
 
-describe Solver do
-  context '#factorial' do
-    it 'returns 1 for factorial of 0' do
-      expect(Solver.factorial(0)).to eq(1)
+RSpec.describe Solver do
+  describe '#factorial' do
+    context 'when input is 0' do
+      it 'returns 1' do
+        expect(described_class.factorial(0)).to eq(1)
+      end
     end
-    it 'returns 1 for factorial of 1' do
-      expect(Solver.factorial(1)).to eq(1)
+    context 'when input is 1' do
+      it 'returns 1' do
+        expect(described_class.factorial(1)).to eq(1)
+      end
     end
-    it 'returns correct factorial for positive integers' do
-      expect(Solver.factorial(5)).to eq(120)
-      expect(Solver.factorial(10)).to eq(3_628_800)
+    context 'when input is a positive integer' do
+      it 'returns factorial of N' do
+        expect(described_class.factorial(5)).to eq(120)
+        expect(described_class.factorial(10)).to eq(3_628_800)
+      end
     end
-    it 'raise an exception for negative integer' do
-      expect { Solver.factorial(-3) }.to raise_error(ArgumentError)
-    end
-  end
-
-  context '#reverse' do
-    it 'returns "olleh" for "hello"' do
-      expect(Solver.reverse('hello')).to eql 'olleh'
-    end
-
-    it 'returns "dlrow" for "world"' do
-      expect(Solver.reverse('world')).to eql 'dlrow'
+    context 'when input is a negative integer' do
+      it 'raise an exception for negative integer' do
+        expect { described_class.factorial(-3) }.to raise_error(ArgumentError)
+      end
     end
   end
 
-  context '#fizzbuzz' do
-    it 'returns "fizz" for 3' do
-      expect(Solver.fizzbuzz(3)).to eql 'fizz'
+  describe '#reverse' do
+    it 'returns "olleh"' do
+      expect(described_class.reverse('hello')).to eql 'olleh'
     end
 
-    it 'returns "buzz" for 5' do
-      expect(Solver.fizzbuzz(5)).to eql 'buzz'
+    it 'returns "dlrow"' do
+      expect(described_class.reverse('world')).to eql 'dlrow'
+    end
+  end
+
+  describe '#fizzbuzz' do
+    it 'returns "fizz"' do
+      expect(described_class.fizzbuzz(3)).to eql 'fizz'
     end
 
-    it 'returns "fizzbuzz" for 15' do
-      expect(Solver.fizzbuzz(15)).to eql 'fizzbuzz'
+    it 'returns "buzz"' do
+      expect(described_class.fizzbuzz(5)).to eql 'buzz'
+    end
+
+    it 'returns "fizzbuzz"' do
+      expect(described_class.fizzbuzz(15)).to eql 'fizzbuzz'
     end
   end
 end
