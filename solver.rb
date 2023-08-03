d285762c4d043889fa060f96c50e3b1604240ec1
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-08-03 11:05:22 +0100

    Implement the factorial method to pass the test

---
+++ solver.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Solver is a utility class that provides various mathematical operations.
+# It currently supports calculating the factorial of a positive integer.
+class Solver
+  # Calculates the factorial of a positive integer.
+  def self.factorial(number)
+    raise ArgumentError, 'Factorial is not defined for negative numbers' if number.negative?
+    return 1 if number.zero?
+
+    result = 1
+    (1..n).each do |num|
+      result *= num
+    end
+    result
+  end
+end
